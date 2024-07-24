c4d16b51edd060e8cc2c95fe89ce9670e16dd1b7
m-cortesleon <marioctrebol@gmail.com>
2024-07-24 03:54:09 -0400

    Handling more FakeStoreApiService error cases.

--- app/src/main/java/cl/test/supertienda/model/product/ProductRepository.kt
+++ app/src/main/java/cl/test/supertienda/model/product/ProductRepository.kt
@@ -15,57 +15,23 @@ class ProductRepository @Inject constructor(private val apiService: FakeStoreApi
      * Obtains a list with all the store products
      * @return [Response] with a [List] with all the store [Product]s, if request is successful.
      */
-    suspend fun getAllProducts(): Response<List<Product>> {
-        val apiResponse = apiService.getProductList()
-        return if (apiResponse.isSuccessful)
-            Response(
-                isSuccessful = true,
-                body = apiResponse.body()!!
-            )
-        else
-            Response(
-                isSuccessful = false,
-                error = "Error ${apiResponse.code()}"
-            )
-    }
+    suspend fun getAllProducts(): Response<List<Product>> =
+        handleApiResponse { apiService.getProductList() }
 
     /**
      * Obtains product data for a given product id
      * @param id Id for the product been requested
      * @return [Response] with the required [Product], if request is successful
      */
-    suspend fun getProduct(id: Int): Response<Product> {
-        val apiResponse = apiService.getProduct(id)
-
-        return if (apiResponse.isSuccessful)
-            Response(
-                isSuccessful = true,
-                body = apiResponse.body()!!
-            )
-        else
-            Response(
-                isSuccessful = false,
-                error = "Error ${apiResponse.code()}"
-            )
-    }
+    suspend fun getProduct(id: Int): Response<Product> =
+        handleApiResponse { apiService.getProduct(id) }
 
     /**
      * Obtains a list with available product categories in the store
      * @return [Response] with the categories [List], if request is successful
      */
-    suspend fun getProductCategories(): Response<List<String>> {
-        val apiResponse = apiService.getCategories()
-        return if (apiResponse.isSuccessful)
-            Response(
-                isSuccessful = true,
-                body = apiResponse.body()!!
-            )
-        else
-            Response(
-                isSuccessful = false,
-                error = "Error ${apiResponse.code()}"
-            )
-    }
+    suspend fun getProductCategories(): Response<List<String>> =
+        handleApiResponse { apiService.getCategories() }
 
     /**
      * Obtains a list with all the store products for a given category
@@ -73,17 +39,37 @@ class ProductRepository @Inject constructor(private val apiService: FakeStoreApi
      * @return [Response] with a [List] with all the store [Product]s for the given category,
      * if request is successful.
      */
-    suspend fun getProductsByCategory(category: String): Response<List<Product>> {
-        val apiResponse = apiService.getProductListByCategory(category)
-        return if (apiResponse.isSuccessful)
-            Response(
-                isSuccessful = true,
-                body = apiResponse.body()!!
-            )
-        else
-            Response(
+    suspend fun getProductsByCategory(category: String): Response<List<Product>> =
+        handleApiResponse { apiService.getProductListByCategory(category) }
+
+    /**
+     * Handles the response to a [FakeStoreApiService] request
+     * @param apiRequest Suspend function that returns a retrofit2.Response<T> element to be handled
+     * @return [Response<T>] object for display data.
+     */
+    private suspend fun <T> handleApiResponse(apiRequest: suspend () -> retrofit2.Response<T>): Response<T> {
+        runCatching {
+            apiRequest()
+        }.onSuccess { apiResponse ->
+            return if (apiResponse.isSuccessful)
+                Response(
+                    isSuccessful = true,
+                    body = apiResponse.body()!!
+                )
+            else
+                Response(
+                    isSuccessful = false,
+                    error = "Error ${apiResponse.code()}"
+                )
+        }.onFailure { e ->
+            return Response(
                 isSuccessful = false,
-                error = "Error ${apiResponse.code()}"
+                error = e.message
             )
+        }
+        return Response(
+            isSuccessful = false,
+            error = "This line should never be reached"
+        )
     }
 }
